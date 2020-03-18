96e493b7808976283d0531af70778033b9e454a3
RazeSoldier <razesoldier@outlook.com>
2020-03-19 00:51:53 +0800

    Only srp.request can access PAP

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'pap',
     'namespace' => 'RazeSoldier\Seat3VPap\Http\Controller',
-    'middleware' => ['web', 'auth', 'locale'],
+    'middleware' => ['web', 'auth', 'locale', 'bouncer:srp.request'],
 ], function () {
     Route::get('/', 'PapController@showMainPage');
     Route::get('/group/{id}', 'PapController@showGroupPap')->name('pap.pap');
