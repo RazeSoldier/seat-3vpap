706a983a12831346da1a0eff713471e146a161a4
RazeSoldier <razesoldier@outlook.com>
2021-01-23 20:18:32 +0800

    Save as file while handing fleet stat

--- src/Http/Controller/FleetStatController.php
+++ src/Http/Controller/FleetStatController.php
@@ -2,7 +2,9 @@
 
 namespace RazeSoldier\Seat3VPap\Http\Controller;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use RazeSoldier\Seat3VPap\Jobs\ImportFleetStat;
 
 class FleetStatController
@@ -23,8 +25,31 @@ class FleetStatController
         abort_if($request->text === null || $request->notice === null
             || $request->point === null, 403);
         $members = explode("\n", $request->text);
+        $this->saveFile($request);
         $job = new ImportFleetStat($members, auth()->user()->main_character->name, $request->point, $request->notice);
         dispatch($job)->onQueue('high');
         return response()->json(['status' => 'ok']);
+    }
+
+    /**
+     * Save the fleet stat to a file
+     * @param Request $request
+     */
+    private function saveFile(Request $request)
+    {
+        $filename = 'fleetstat-' . Carbon::now()->format('YmdHis') . '-' . $request->notice . '.txt';
+        $time = Carbon::now()->addHours(8)->format('Y-m-d H:i:s');
+        $username = auth()->user()->main_character->name;
+
+        $text = <<<TEXT
+Time: $time
+FC: $username
+Point: $request->point
+Notice: $request->notice
+Member:
+$request->text
+TEXT;
+
+        Storage::disk('local')->put("fleetstat/$filename", $text);
     }
 }
--- src/PapServiceProvider.php
+++ src/PapServiceProvider.php
@@ -89,6 +89,6 @@ class PapServiceProvider extends AbstractSeatPlugin
      */
     public function getVersion(): string
     {
-        return '0.5.6';
+        return '0.5.7';
     }
 }
